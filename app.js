91c40b8ce622d40c02127e6d52110350ca26eb01
gent0807 <theloopholesnk@gmail.com>
2023-12-13 23:34:34 +0900

    chat gpt api를 통한 답변 획득 가능

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 const express = require("express");
 const path = require("path");
+const OpenAI=require("openai");
+
+const openai = new OpenAI({
+  apiKey: "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"
+});
 
 const app = express();
+
+const request = require("request");
+const client_id = "F4pmcTtDerRWGJpqxc1n";
+const clent_secret= "KKjtYCtefP";
 
 app.set("port", process.env.PORT || 5000);
 
@@ -11,6 +20,63 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "/my_chat_bot/build/index.html"));
 });
 
+//입력창에 입력된 한국어 문장을 파파고 api를 이용해 영어로 번역해주는 api
+app.get('/translateToEnglish', async(req, res)=>{
+  const api_url='https://openapi.naver.com/v1/papago/n2mt';
+  const query=req.query.prompt;
+
+  const options={
+    url: api_url,
+    form: {'source':'ko', 'target':'en', 'text':query},
+    headers: {'X-Naver-Client-Id': client_id, 'X-Naver-Client-Secret': clent_secret}
+  };
+
+  request.post(options, function(error, response, body){
+    if (!error && response.statusCode == 200) {
+      res.json(body);
+    } else {
+      console.log('error = ' + response.statusCode);
+    }
+  });
+
+  
+});
+
+//영어로 번역된 문장을 openai api를 이용해 답변을 받아오는 api
+app.get('/chat', async(req, res)=>{
+  const prompt = req.query.prompt;
+
+  const response = await openai.chat.completions.create({
+    model: 'gpt-3.5-turbo',
+    messages:[
+      {"role":"user", "content": prompt}
+    ],
+    max_tokens: 256,
+  });
+  
+  res.send(response.choices[0].message.content);
+});
+
+//openai api를 이용해 받아온 영어 답변을 파파고 api를 이용해 한국어로 번역해주는 api
+app.get('/translateToKorean', async(req, res)=>{
+  const api_url='https://openapi.naver.com/v1/papago/n2mt';
+  const query=req.query.prompt;
+
+  const options={
+    url: api_url,
+    form: {'source':'en', 'target':'ko', 'text':query},
+    headers: {'X-Naver-Client-Id':client_id, 'X-Naver-Client-Secret':clent_secret}
+  };
+
+  request.post(options, function(error, response, body){
+    if (!error && response.statusCode == 200) {
+      res.json(body);
+    } else {
+      console.log('error = ' + response.statusCode);
+    }
+  });
+});
+
 app.listen(app.get("port"), () => {
-  console.log(app.get("port"), "번 포트에서 대기중..");
+  console.log(app.get("port"), "빈 포트에서 대기중..");
 });
--- my_chat_bot/src/Chat/ChatUI.js
+++ my_chat_bot/src/Chat/ChatUI.js
@@ -1,7 +1,6 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import './ChatUI.css';
 import axios from 'axios';
-import {Configuration, OpenAIApi} from 'https://cdn.skypack.dev/openai';
 
 const ChatUI= ()=> {
   const [inputValue, setInputValue] = useState(''); // input 창에 입력된 내용  
@@ -9,29 +8,40 @@ const ChatUI= ()=> {
   const templateFront=`<div class="line"><span class="chat-box mine">`; // 추가되는 채팅의 html 형식 고정 앞부분
   const templateBack='</span></div>'; // 추가되는 채팅의 html 형식 고정 뒷부분
 
-  const addChat = () => {
+  //전의 대화 누적
+  let storedInputValue=useRef(inputValue); // input 창에 입력된 내용을 저장하는 변수
+
+  const addChat = async() => {
+
+    //이전에 입력한 내용을 합친 내용을 저장
+    storedInputValue.current=storedInputValue.current+" "+inputValue; // input 창에 입력된 내용을 저장
+
+    //입력창에 입력된 내용을 채팅창에 추가
     let template=templateFront+inputValue+templateBack;
     let chatContent = document.querySelector('.chat-content');
     chatContent.insertAdjacentHTML('beforeend', template);
     setInputValue(''); 
+
+    //파파고 api를 이용해 영어로 번역된 값 받아오기
+    // const english= await axios.get('https://localhost:5000/transleateToEnglish', {
+    //   params: { prompt: storedInputValue.current },
+    // });
     
-    const configuration=new Configuration({
-      apiKey: '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',
+    // axios를 이용해 서버에 get 요청을 보내고, 응답을 받아온다.
+    const response = await axios.get('http://localhost:5000/chat', {
+      params: { prompt: inputValue },
     });
 
-    const openai=new OpenAIApi(configuration);
+    //파파고 api를 이용해 영어 답변을 한국어로 번역해 받아오기
+    // const finalResponse= await axios.get('https://localhost:5000/transleateToKorean', {
+    //   params: { prompt: response.data },
+    // })
 
-    openai.createCompletion({
-      model:"text-davinci-002",
-      prompt:inputValue,
+    let front=`<div class="line"><span class="chat-box">`;
+    let back='</span></div>';
+    template=front+response.data+back;
+    chatContent.insertAdjacentHTML('beforeend', template);
 
-    }).then((result)=>{
-      console.log(result.data.choices[0].text);
-      let front=`<div class="line"><span class="chat-box"></span>`;
-      let back='</span></div>';
-      template=front+result.data.choices[0].text+back;
-      chatContent.insertAdjacentHTML('beforeend', template);
-    })
   
 
   };
